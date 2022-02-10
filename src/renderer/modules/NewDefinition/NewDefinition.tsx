848cb1b3c2c44a190aa55ecc5c1dfb5e5120e257
Wojciech Malarski <w.malarski@gmail.com>
2022-02-10 22:29:08 +0100

    object list as links

--- src/renderer/modules/NewDefinition/NewDefinition.tsx
+++ src/renderer/modules/NewDefinition/NewDefinition.tsx
@@ -1,7 +1,8 @@
 import { Text } from "@nextui-org/react";
+import { getSnapshot } from "mobx-state-tree";
 import { ReactElement, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-location";
+import { Link, useSearch } from "react-location";
 import { IntroLayout } from "renderer/components/IntroLayout/IntroLayout";
 import { ProjectDefinition } from "renderer/models/definition";
 import { routePaths } from "renderer/utils/routes";
@@ -12,13 +13,22 @@ import { ObjectsList } from "./ObjectsList/ObjectsList";
 export const NewDefinition = (): ReactElement => {
   const { t } = useTranslation("definition");
 
+  const { objectId } = useSearch();
+
   const [definition] = useState(() => {
     return ProjectDefinition.create({
       name: t("defaultDefinitionName"),
     });
   });
 
-  const objectDefinition = definition.objects.at(0);
+  const objectDefinition =
+    definition.objects.find((object) => object.id === objectId) ??
+    definition.objects.at(0);
+
+  console.log({
+    objectId,
+    d: objectDefinition && getSnapshot(objectDefinition),
+  });
 
   return (
     <IntroLayout>
--- src/renderer/modules/NewDefinition/ObjectsList/ObjectsListItem/ObjectsListItem.tsx
+++ src/renderer/modules/NewDefinition/ObjectsList/ObjectsListItem/ObjectsListItem.tsx
@@ -2,7 +2,9 @@ import { Col, Text } from "@nextui-org/react";
 import { observer } from "mobx-react-lite";
 import { Instance } from "mobx-state-tree";
 import { ReactElement } from "react";
+import { Link } from "react-location";
 import { ObjectDefinition } from "renderer/models/definition";
+import { routePaths } from "renderer/utils/routes";
 
 type Props = {
   objectDefinition: Instance<typeof ObjectDefinition>;
@@ -11,12 +13,17 @@ type Props = {
 export const ObjectsListItem = observer(
   ({ objectDefinition }: Props): ReactElement => {
     return (
-      <Col>
-        <Text>{objectDefinition.name}</Text>
-        <Text small color="$accents7">
-          {objectDefinition.description}
-        </Text>
-      </Col>
+      <Link
+        to={routePaths.newDefinition}
+        search={{ objectId: objectDefinition.id }}
+      >
+        <Col>
+          <Text>{objectDefinition.name}</Text>
+          <Text small color="$accents7">
+            {objectDefinition.description}
+          </Text>
+        </Col>
+      </Link>
     );
   }
 );
