aedcf4782afa66883a498577d4c90884e051b30e
Wojciech Malarski <w.malarski@gmail.com>
2022-03-17 20:58:07 +0100

    workspace initial layout added

--- src/renderer/pages/Workspace/WorkspaceLayout/WorkspaceLayout.tsx
+++ src/renderer/pages/Workspace/WorkspaceLayout/WorkspaceLayout.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@geist-ui/core";
 import * as FlexLayout from "flexlayout-react";
-import { Instance } from "mobx-state-tree";
+import { observer } from "mobx-react-lite";
+import { getSnapshot, Instance } from "mobx-state-tree";
 import { ReactElement, ReactNode, useCallback, useState } from "react";
 import { WorkspaceStore } from "renderer/models";
 import "./dark.css";
@@ -11,40 +11,32 @@ type Props = {
   workspaceStore: Instance<typeof WorkspaceStore>;
 };
 
-export const WorkspaceLayout = ({ workspaceStore }: Props): ReactElement => {
-  const [model, setModel] = useState(getDefaultModel);
+export const WorkspaceLayout = observer(
+  ({ workspaceStore }: Props): ReactElement => {
+    const projectSnapshot = getSnapshot(workspaceStore.project);
+    const [model, setModel] = useState(() => {
+      return getDefaultModel(projectSnapshot);
+    });
 
-  const factory = useCallback(
-    (node: FlexLayout.TabNode): ReactNode => {
-      const component = node.getComponent();
-      switch (component) {
-        case "button": {
-          return (
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                border: "1px solid gray",
-              }}
-            >
-              <Button>{node.getName()}</Button>
-            </div>
-          );
+    const factory = useCallback(
+      (node: FlexLayout.TabNode): ReactNode => {
+        const component = node.getComponent();
+        switch (component) {
+          case LayoutNodeKind.Items:
+            return <ItemsList workspaceStore={workspaceStore} node={node} />;
+          default:
+            return <p>AA</p>;
         }
-        case LayoutNodeKind.ItemsList:
-          return <ItemsList workspaceStore={workspaceStore} node={node} />;
-        default:
-          return <p>AA</p>;
-      }
-    },
-    [workspaceStore]
-  );
+      },
+      [workspaceStore]
+    );
 
-  return (
-    <FlexLayout.Layout
-      onModelChange={setModel}
-      model={model}
-      factory={factory}
-    />
-  );
-};
+    return (
+      <FlexLayout.Layout
+        onModelChange={setModel}
+        model={model}
+        factory={factory}
+      />
+    );
+  }
+);
--- src/renderer/pages/Workspace/WorkspaceLayout/WorkspaceLayout.utils.ts
+++ src/renderer/pages/Workspace/WorkspaceLayout/WorkspaceLayout.utils.ts
@@ -1,40 +1,67 @@
 import * as FlexLayout from "flexlayout-react";
+import { IJsonTabSetNode } from "flexlayout-react";
+import { SnapshotOut } from "mobx-state-tree";
+import { Project } from "renderer/models/project/Project";
+import i18n from "renderer/utils/i18next";
 
 export enum LayoutNodeKind {
-  ItemsList = "ItemsList",
+  Items = "Items",
+  Timeline = "Timeline",
+  Video = "Video",
+  Comments = "Comments",
 }
 
-export const getDefaultModel = () => {
+export const getDefaultModel = (project: SnapshotOut<typeof Project>) => {
   return FlexLayout.Model.fromJson({
     global: {},
-    borders: [],
+    borders: [
+      {
+        type: "border",
+        location: "bottom",
+        children: [
+          {
+            type: "tab",
+            name: i18n.t<string>("timelineNode", { ns: "workspace" }),
+            component: LayoutNodeKind.Timeline,
+          },
+        ],
+      },
+      {
+        type: "border",
+        location: "right",
+        children: [
+          {
+            type: "tab",
+            name: i18n.t<string>("itemsNode", { ns: "workspace" }),
+            component: LayoutNodeKind.Items,
+          },
+          {
+            type: "tab",
+            name: i18n.t<string>("commentsNode", { ns: "workspace" }),
+            component: LayoutNodeKind.Comments,
+          },
+        ],
+      },
+    ],
     layout: {
       type: "row",
       weight: 200,
-      children: [
-        {
+      children: project.resources.map(
+        (resource): IJsonTabSetNode => ({
           type: "tabset",
           weight: 100,
           children: [
             {
               type: "tab",
-              name: "One",
-              component: "button",
+              name: resource.path,
+              component: LayoutNodeKind.Video,
+              enableClose: false,
+              id: resource.id,
+              config: resource,
             },
           ],
-        },
-        {
-          type: "tabset",
-          weight: 100,
-          children: [
-            {
-              type: "tab",
-              name: "Items List",
-              component: LayoutNodeKind.ItemsList,
-            },
-          ],
-        },
-      ],
+        })
+      ),
     },
   });
 };
