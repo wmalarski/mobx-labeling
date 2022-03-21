f99d18ec59fd86ba5e8a5b83d8856e0a767746c0
Wojciech Malarski <w.malarski@gmail.com>
2022-03-21 21:12:58 +0100

    TextForm unit tests

---
+++ src/renderer/pages/Workspace/WorkspaceLayout/ItemsList/ItemsListItem/FieldListItem/TextForm/TextForm.test.tsx
@@ -0,0 +1,102 @@
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SnapshotIn, types } from "mobx-state-tree";
+import { ComponentProps } from "react";
+import { TextDefinition, TextField } from "renderer/models";
+import { PropsWithTestWrapper, TestWrapper } from "renderer/tests/Wrapper";
+import i18n from "renderer/utils/i18next";
+import { TextForm } from "./TextForm";
+
+type Props = ComponentProps<typeof TextForm>;
+
+const Model = types.model({
+  definition: TextDefinition,
+  field: TextField,
+});
+
+const getInstance = ({
+  definition,
+  field,
+}: {
+  definition?: Partial<SnapshotIn<typeof TextDefinition>>;
+  field?: Partial<SnapshotIn<typeof TextField>>;
+} = {}) => {
+  return Model.create({
+    definition: {
+      name: "Text",
+      change: "EveryFrame",
+      default: "Hello",
+      description: "Description",
+      id: "id",
+      kind: "Text",
+      ...definition,
+    },
+    field: {
+      currentFrame: 1,
+      definition: "id",
+      id: "1",
+      kind: "Text",
+      values: { "1": { value: "hello" } },
+      ...field,
+    },
+  });
+};
+
+const renderComponent = ({
+  wrapperProps,
+  ...props
+}: PropsWithTestWrapper<Partial<Props>> = {}) => {
+  const instance = getInstance();
+  const defaultProps: Props = {
+    textField: instance.field,
+  };
+
+  return render(
+    <TestWrapper {...wrapperProps}>
+      <TextForm {...defaultProps} {...props} />
+    </TestWrapper>
+  );
+};
+
+describe("<TextForm />", () => {
+  it("should render", async () => {
+    expect.hasAssertions();
+
+    renderComponent();
+
+    await expect(screen.findByLabelText("Text")).resolves.toBeInTheDocument();
+  });
+
+  it("should render error message", async () => {
+    expect.hasAssertions();
+
+    const instance = getInstance({
+      field: { values: {} },
+    });
+
+    renderComponent({
+      textField: instance.field,
+    });
+
+    const header = i18n.t<string>("invalidField", { ns: "workspace" });
+    await expect(screen.findByText(header)).resolves.toBeInTheDocument();
+  });
+
+  it("should change value", async () => {
+    expect.hasAssertions();
+
+    const instance = getInstance();
+
+    renderComponent({
+      textField: instance.field,
+    });
+
+    const input = await screen.findByLabelText(instance.definition.name);
+    userEvent.clear(input);
+    userEvent.type(input, "world");
+
+    expect(instance.field.current?.value).toBe("world");
+  });
+});
--- src/renderer/pages/Workspace/WorkspaceLayout/ItemsList/ItemsListItem/FieldListItem/TextForm/TextForm.tsx
+++ src/renderer/pages/Workspace/WorkspaceLayout/ItemsList/ItemsListItem/FieldListItem/TextForm/TextForm.tsx
@@ -1,26 +1,37 @@
-import { Input } from "@geist-ui/core";
+import { Input, Text } from "@geist-ui/core";
 import { observer } from "mobx-react-lite";
 import { Instance } from "mobx-state-tree";
 import { ChangeEvent, ReactElement } from "react";
+import { useTranslation } from "react-i18next";
 import { TextField } from "renderer/models";
 
 type Props = {
   textField: Instance<typeof TextField>;
 };
 
-export const TextForm = observer(({ textField }: Props): ReactElement => {
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    textField.current?.setValue(event.target.value);
-  };
+export const TextForm = observer(
+  ({ textField }: Props): ReactElement | null => {
+    const { t } = useTranslation("workspace");
 
-  return (
-    <Input
-      value={textField.current?.value}
-      onChange={handleChange}
-      aria-label={textField.definition.name}
-      placeholder={textField.definition.name}
-      width="100%"
-      clearable
-    />
-  );
-});
+    const current = textField.current;
+
+    if (!current) {
+      return <Text type="error">{t("invalidField")}</Text>;
+    }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      current.setValue(event.target.value);
+    };
+
+    return (
+      <Input
+        value={current.value}
+        onChange={handleChange}
+        aria-label={textField.definition.name}
+        placeholder={textField.definition.name}
+        width="100%"
+        clearable
+      />
+    );
+  }
+);
